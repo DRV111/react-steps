4cb7d27994b54c5c408be37f959c68acd2c3197f
DRV111 <xblackcrownx@gmail.com>
2023-10-10 12:57:40 +0300

    feat: add handler function

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './index.css';
 
 const messages = [
@@ -7,17 +8,32 @@ const messages = [
 ];
 
 function App() {
+  const [step, setStep] = useState(1);
+  function handlePrev() {
+    if (step === 1) {
+      return;
+    }
+    setStep(step - 1);
+  }
+  function handleNext() {
+    if (step === messages.length) {
+      return;
+    }
+    setStep(step + 1);
+  }
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
-      <p className="message">Shit!</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={handlePrev}>Previous</button>
+        <button onClick={handleNext}>Next</button>
       </div>
     </div>
   );
